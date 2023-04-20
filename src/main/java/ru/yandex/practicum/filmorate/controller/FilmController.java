ef26d7994d4e4f8e3a24c90f886cc8c7b22915e9
Canoutis <rosoutis@gmail.com>
2023-04-20 13:31:20 +0300

    fix: version conflict fix v4

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,8 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.exception.FilmSaveException;
@@ -27,7 +27,7 @@ public class FilmController {
         return new ArrayList<>(hmFilms.values());
     }
 
-    @PostMapping(value = "/film")
+    @PostMapping(value = "/films")
     public Film create(@Valid @RequestBody Film film) {
         if (film.getName().isEmpty() || film.getDescription().length() > 200
                 || film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))
@@ -41,7 +41,7 @@ public class FilmController {
         }
     }
 
-    @PatchMapping(value = "/film")
+    @PutMapping(value = "/films")
     public Film update(@Valid @RequestBody Film film) {
         if (film.getName().isEmpty() || film.getDescription().length() > 200
                 || film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,8 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.exception.UserSaveException;
@@ -26,7 +26,7 @@ public class UserController {
         return new ArrayList<>(hmUsers.values());
     }
 
-    @PostMapping(value = "/user")
+    @PostMapping(value = "/users")
     public User create(@Valid @RequestBody User user) {
         if (!user.getEmail().contains("@") || user.getLogin().isEmpty() || user.getLogin().contains(" ")
                 || user.getBirthday().isAfter(LocalDate.now())) {
@@ -40,9 +40,9 @@ public class UserController {
         }
     }
 
-    @PatchMapping(value = "/user")
+    @PutMapping(value = "/users")
     public User update(@Valid @RequestBody User user) {
-        if (user.getEmail().contains("@") || user.getLogin().isEmpty() || user.getLogin().contains(" ")
+        if (!user.getEmail().contains("@") || user.getLogin().isEmpty() || user.getLogin().contains(" ")
                 || user.getBirthday().isAfter(LocalDate.now())) {
             log.warn("Ошибка обновления пользователя с id={}. Ошибка входных данных! " + user, user.getId());
             throw new UserSaveException("Ошибка создания пользователя. Ошибка входных данных!");
