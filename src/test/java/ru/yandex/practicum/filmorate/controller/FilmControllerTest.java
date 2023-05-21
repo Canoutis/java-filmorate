cf06a5036587a3bc12a23e605fec7408eb8ca2db
Canoutis <rosoutis@gmail.com>
2023-05-21 18:14:27 +0300

    fix: reviewer comments v2.1

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.exception.ObjectSaveException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootTest
 public class FilmControllerTest {
@@ -20,56 +20,7 @@ public class FilmControllerTest {
     }
 
     @Test
-    void shouldThrowExceptionWithEmptyName() {
-        Film film = Film.builder()
-                .name("")
-                .description("description")
-                .duration(114)
-                .releaseDate(LocalDate.of(2011, 3, 10))
-                .build();
-        Assertions.assertThrows(ObjectSaveException.class, () -> controller.create(film));
-    }
-
-    @Test
-    void shouldThrowExceptionWithLongDescription() {
-        Film film = Film.builder()
-                .name("The Lincoln Lawyer")
-                .description("Микки Холлер – блестящий и удачливый адвокат из Лос-Анджелеса, чей яркий имидж и образ " +
-                        "жизни отлично дополняет его любимая машина «Линкольн». Очередное дело – нападение богатого " +
-                        "клиента Луи Руле на проститутку сначала казалось легким, и он без труда добился " +
-                        "оправдательного приговора для подзащитного. Но вскоре Микки понимает, что его клиент " +
-                        "скрывает правду. Пока Холлер пытается вывести Руле на чистую воду, его подставляют " +
-                        "по-крупному, и теперь уже угроза тюремного заключения нависает над самим Холлером.")
-                .duration(114)
-                .releaseDate(LocalDate.of(2011, 3, 10))
-                .build();
-        Assertions.assertThrows(ObjectSaveException.class, () -> controller.create(film));
-    }
-
-    @Test
-    void shouldThrowExceptionWithIncorrectDuration() {
-        Film film = Film.builder()
-                .name("Film")
-                .description("Description")
-                .duration(0)
-                .releaseDate(LocalDate.of(2011, 3, 10))
-                .build();
-        Assertions.assertThrows(ObjectSaveException.class, () -> controller.create(film));
-    }
-
-    @Test
-    void shouldThrowExceptionWithUnbelievableReleaseDate() {
-        Film film = Film.builder()
-                .name("Film")
-                .description("Description")
-                .duration(114)
-                .releaseDate(LocalDate.of(1600, 3, 10))
-                .build();
-        Assertions.assertThrows(ObjectSaveException.class, () -> controller.create(film));
-    }
-
-    @Test
-    void shouldBeOkWithCorrectFilm() {
+    void shouldBeOkCreateCorrectFilm() {
         Film film = Film.builder()
                 .name("Film")
                 .description("Description")
@@ -77,10 +28,40 @@ public class FilmControllerTest {
                 .releaseDate(LocalDate.of(2006, 3, 10))
                 .build();
         Film savedFilm = controller.create(film);
-        Assertions.assertEquals(1, controller.findAll().size());
         Assertions.assertEquals(film.getName(), savedFilm.getName());
         Assertions.assertEquals(film.getDescription(), savedFilm.getDescription());
         Assertions.assertEquals(film.getDuration(), savedFilm.getDuration());
         Assertions.assertEquals(film.getReleaseDate(), savedFilm.getReleaseDate());
+    }
+
+    @Test
+    void shouldBeOkUpdateCorrectFilm() {
+        Film film = Film.builder()
+                .name("Film")
+                .description("Description")
+                .duration(114)
+                .releaseDate(LocalDate.of(2006, 3, 10))
+                .build();
+        Film savedFilm = controller.create(film);
+        savedFilm.setName("New Film");
+        Film updatedFilm = controller.update(savedFilm);
+        Assertions.assertEquals(savedFilm.getName(), updatedFilm.getName());
+        Assertions.assertEquals(savedFilm.getDescription(), updatedFilm.getDescription());
+        Assertions.assertEquals(savedFilm.getDuration(), updatedFilm.getDuration());
+        Assertions.assertEquals(savedFilm.getReleaseDate(), updatedFilm.getReleaseDate());
+    }
+
+    @Test
+    void shouldGetCorrectFilmsList() {
+        List<Film> films = controller.findAll();
+        Film film = Film.builder()
+                .name("Film 2")
+                .description("Description 2")
+                .duration(220)
+                .releaseDate(LocalDate.of(2004, 3, 10))
+                .build();
+        controller.create(film);
+        List<Film> films2 = controller.findAll();
+        Assertions.assertEquals(films.size() + 1, films2.size());
     }
 }
