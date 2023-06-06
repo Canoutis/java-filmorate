c219e967647895bfd3a72fd7332cfe0f1eaccfe1
noir74y <noir74@yandex.ru>
2023-06-07 00:55:36 +0300

    feat: implement createReview

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @Builder
@@ -20,4 +22,15 @@ public class Review {
     @NotNull
     private Integer filmId;
     private Integer useful;
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> values = new HashMap<>();
+        values.put("review_id", reviewId);
+        values.put("content", content);
+        values.put("is_positive", isPositive);
+        values.put("user_id", userId);
+        values.put("film_id", filmId);
+        values.put("useful", useful);
+        return values;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDaoImpl.java
@@ -1,17 +1,35 @@
 package ru.yandex.practicum.filmorate.storage.review;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
 import java.util.List;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ReviewDaoImpl implements ReviewDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final UserDbStorage userDbStorage;
+    private final FilmDbStorage filmDbStorage;
+
     @Override
     public Review createReview(Review review) {
-        return null;
+        userDbStorage.getUserById(review.getUserId());
+        filmDbStorage.getFilmById(review.getFilmId());
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("review")
+                .usingGeneratedKeyColumns("review_id");
+        review.setReviewId(simpleJdbcInsert.executeAndReturnKey(review.toMap()).intValue());
+        review.setUseful(0);
+        return review;
     }
 
     @Override
