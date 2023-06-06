b795eba575ec3eece8495d4f6354b4f66555d0df
noir74y <noir74@yandex.ru>
2023-06-07 00:29:11 +0300

    fix: change some annotations

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 
 @Data
 @Builder
@@ -17,11 +16,8 @@ public class Review {
     @NotNull
     private Boolean isPositive;
     @NotNull
-    @Positive
     private Integer userId;
     @NotNull
-    @Positive
     private Integer filmId;
-    @Positive
     private Integer useful;
 }
