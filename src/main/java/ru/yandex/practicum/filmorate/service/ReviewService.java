a1d7a62369036b9699b1989c40a405fea6a725d3
noir74y <noir74@yandex.ru>
2023-06-06 22:57:59 +0300

    feat: ReviewService template

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -2,10 +2,48 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.storage.review.ReviewDaoImpl;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewDaoImpl reviewDao;
+
+    public Review createReview(Review review) {
+        return null;
+    }
+
+    public Review updateReview(Review review) {
+        return null;
+    }
+
+    public void removeReview(Integer reviewId) {
+    }
+
+    public Review findReview(Integer reviewId){
+        return null;
+    }
+
+    public List<Review> findReviews(Integer filmId, Integer limit) {
+        return null;
+    }
+
+    public void addLikeToReview(Integer reviewId, Integer userId) {
+
+    }
+
+    public void addLDislikeToReview(Integer reviewId, Integer userId) {
+
+    }
+
+    public void removeLikeFromReview(Integer reviewId, Integer userId) {
+
+    }
+
+    public void removeDislikeFromReview(Integer reviewId, Integer userId) {
+
+    }
 }
