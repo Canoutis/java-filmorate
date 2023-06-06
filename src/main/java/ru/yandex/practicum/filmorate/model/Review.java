275c5c00f50f18a8ff1919742e9ba1f7de3a8af7
noir74y <noir74@yandex.ru>
2023-06-07 00:12:38 +0300

    feat: Review class implementation

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -1,4 +1,27 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class Review {
+    private Integer reviewId;
+    @NotNull
+    private String content;
+    @NotNull
+    private Boolean isPositive;
+    @NotNull
+    @Positive
+    private Integer userId;
+    @NotNull
+    @Positive
+    private Integer filmId;
+    @Positive
+    private Integer useful;
 }
