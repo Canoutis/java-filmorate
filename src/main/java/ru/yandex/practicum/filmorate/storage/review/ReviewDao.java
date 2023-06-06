2db14fd80dcd3b454b69fc18d3ac1a0e8800ab58
noir74y <noir74@yandex.ru>
2023-06-06 23:07:43 +0300

    feat: ReviewDao interface

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDao.java
@@ -1,4 +1,27 @@
 package ru.yandex.practicum.filmorate.storage.review;
 
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.List;
+
 public interface ReviewDao {
+
+
+    Review createReview(Review review);
+
+    Review updateReview(Review review);
+
+    void removeReview(Integer reviewId);
+
+    Review findReview(Integer reviewId);
+
+    List<Review> findReviews(Integer filmId, Integer limit);
+
+    void addLikeToReview(Integer reviewId, Integer userId);
+
+    void addLDislikeToReview(Integer reviewId, Integer userId);
+
+    void removeLikeFromReview(Integer reviewId, Integer userId);
+
+    void removeDislikeFromReview(Integer reviewId, Integer userId);
 }
